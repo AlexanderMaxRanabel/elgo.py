e65a67e245aad8494dd1951b9dd77559f8a05921
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-03-03 23:40:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 import requests
+import random
+import wikipedia
+
 intents = discord.Intents.default()
 intents.members = True
 
@@ -28,6 +31,28 @@ async def urban(ctx, term):
                 await ctx.send(f"No definitions found for {term}.")
         else:
             await ctx.send("Error: Failed to fetch data from the Urban Dictionary API.")
+
+@bot.command()
+async def roll(ctx, maximum_number: int):
+    try:
+        result = random.randint(1, maximum_number)
+        await ctx.send(f"You rolled a {result}!")
+    except ValueError:
+        await ctx.send("Invalid argument. Please enter a valid integer.")
+
+@bot.command()
+async def wiki(ctx, *, query: str):
+    try:
+        summary = wikipedia.summary(query)
+        response_str = f"Here's what I found on Wikipedia:\n{summary}"
+        await ctx.send(response_str)
+    except wikipedia.exceptions.PageError:
+        await ctx.send("Error: Page not found on Wikipedia.")
+        #try:
+            #summary = wikipedia.summary(query)
+            #response_str = f""
+
+
 
 #@bot.command()
 #async def levget(ctx):
